4abd46531fd13e35d27160bfb540518c1780bf3d
Patrick Clark Johnson <johnsonp8@nku.edu>
2026-02-17 17:52:28 -0500

    imports and new test to set if database is connected

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy import text
+from database import get_db
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
 @app.get("/health")
 async def root():
     return {"status": "healthy"}
+
+@app.get("/db-test")
+async def db_test(db: Session = Depends(get_db)):
+    result = db.execute(text("SELECT 1"))
+    return {"db_connected": True}
